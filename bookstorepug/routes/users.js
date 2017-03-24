ab93fb184a0ce44206850deffda111f4f3441bc2
eobaah <eobaah@gmail.com>
2017-03-24 11:28:15 -0700

    got sign up form working

--- bookstorepug/routes/users.js
+++ bookstorepug/routes/users.js
@@ -1,9 +1,16 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/', function(request, response, next) {
+  response.send('respond with a resource');
 });
+
+
+router.get('/signup', function(request, response, next) {
+  response.render('signup');
+});
+
+
 
 module.exports = router;
